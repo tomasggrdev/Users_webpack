fb35e982aaf85469c96c2d5be3d8980802db021c
Tomas Gonzalez <tomasggrdev@gmail.com>
2021-07-27 11:02:15 -0500

    subiendo la web

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import Template from './templates/Template.js';
-console.log('hola');
-
+import Template from '@templates/Template.js';
+import '@styles/main.css';
 (async function App() {
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
